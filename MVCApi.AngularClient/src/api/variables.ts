08d9c696e63eddca95aa68d834ea75d34d9b300e
Kubeva <kubeva171@gmail.com>
2026-03-07 19:20:15 +0100

    It now shows error when logging in.

--- MVCApi.AngularClient/src/api/variables.ts
+++ MVCApi.AngularClient/src/api/variables.ts
@@ -1,5 +1,6 @@
 import { InjectionToken } from '@angular/core';
 
+export const emailRegex = /^[a-z\.\-_0-9]+@[a-z0-9]+\.[a-z0-9]+$/;
 export const BASE_PATH = new InjectionToken<string>('basePath');
 export const COLLECTION_FORMATS = {
     'csv': ',',
