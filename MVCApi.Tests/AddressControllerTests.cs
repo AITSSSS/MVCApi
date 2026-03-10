caebd9e2d12b9940788c85757316972d06b28ed8
Pabilo8 <pabilo1.PG@gmail.com>
2026-03-10 22:48:47 +0100

    Added tests for AddressController and CartController

---
+++ MVCApi.Tests/AddressControllerTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using MVCApi.Application.Commands;
+using MVCApi.Application.Dto;
+using MVCApi.Application.Queries;
+using MVCApi.Controllers;
+
+namespace MVCApi.Tests;
+
+public class AddressControllerTests
+{
+    private readonly Mock<IMediator> _mediatorMock;
+    private readonly AddressController _controller;
+
+    public AddressControllerTests()
+    {
+        _mediatorMock = new Mock<IMediator>();
+        _controller = new AddressController(_mediatorMock.Object);
+    }
+
+    [Fact]
+    public async Task GetAddressById_ShouldReturnAddressDto()
+    {
+        var expected = new AddressDto();
+        var id = Guid.NewGuid();
+        _mediatorMock.Setup(m => m.Send(It.Is<GetAddressById>(q => q.AddressId == id), default))
+            .ReturnsAsync(expected);
+
+        var result = await _controller.GetAddressById(id);
+
+        var ok = Assert.IsType<OkObjectResult>(result.Result);
+        Assert.Equal(expected, ok.Value);
+    }
+
+    [Fact]
+    public async Task EditAddress_ShouldReturnGuid()
+    {
+        var expected = Guid.NewGuid();
+        var id = Guid.NewGuid();
+        var command = new EditAddress();
+        _mediatorMock.Setup(m => m.Send(It.Is<EditAddress>(c => c.Id == id), default))
+            .ReturnsAsync(expected);
+
+        var result = await _controller.EditAddress(id, command);
+
+        var ok = Assert.IsType<OkObjectResult>(result.Result);
+        Assert.Equal(expected, ok.Value);
+    }
+}
---
+++ MVCApi.Tests/CartControllerTests.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using MVCApi.Application.Commands;
+using MVCApi.Application.Dto;
+using MVCApi.Application.Queries;
+using MVCApi.Controllers;
+
+namespace MVCApi.Tests;
+
+public class CartControllerTests
+{
+    private readonly Mock<IMediator> _mediatorMock;
+    private readonly CartController _controller;
+
+    public CartControllerTests()
+    {
+        _mediatorMock = new Mock<IMediator>();
+        _controller = new CartController(_mediatorMock.Object);
+    }
+
+    [Fact]
+    public async Task AddProductToCart_ShouldReturnGuid()
+    {
+        var expected = Guid.NewGuid();
+        var command = new AddProductToCart();
+        _mediatorMock.Setup(m => m.Send(It.IsAny<AddProductToCart>(), default))
+            .ReturnsAsync(expected);
+
+        var result = await _controller.AddProductToCart(command);
+
+        var ok = Assert.IsType<OkObjectResult>(result.Result);
+        Assert.Equal(expected, ok.Value);
+    }
+
+    [Fact]
+    public async Task CreateCart_ShouldReturnGuid()
+    {
+        var expected = Guid.NewGuid();
+        _mediatorMock.Setup(m => m.Send(It.IsAny<CreateCart>(), default))
+            .ReturnsAsync(expected);
+
+        var result = await _controller.CreateCart(new CreateCart());
+
+        var ok = Assert.IsType<OkObjectResult>(result.Result);
+        Assert.Equal(expected, ok.Value);
+    }
+
+    [Fact]
+    public async Task GetCartById_ShouldReturnShoppingCartDto()
+    {
+        var expected = new ShoppingCartDto();
+        var id = Guid.NewGuid();
+        var currencyCode = "USD";
+        _mediatorMock.Setup(m =>
+                m.Send(It.Is<GetCartById>(q => q.CartId == id && q.CurrencyCode == currencyCode), default))
+            .ReturnsAsync(expected);
+
+        var result = await _controller.GetCartById(id, currencyCode);
+
+        var ok = Assert.IsType<OkObjectResult>(result.Result);
+        Assert.Equal(expected, ok.Value);
+    }
+
+    [Fact]
+    public async Task ChangeProductCount_ShouldReturnGuid()
+    {
+        var expected = Guid.NewGuid();
+        var command = new ChangeProductCountInCart();
+        _mediatorMock.Setup(m => m.Send(It.IsAny<ChangeProductCountInCart>(), default))
+            .ReturnsAsync(expected);
+
+        var result = await _controller.ChangeProductCount(command);
+
+        var ok = Assert.IsType<OkObjectResult>(result.Result);
+        Assert.Equal(expected, ok.Value);
+    }
+
+    [Fact]
+    public async Task RemoveProduct_ShouldReturnGuid()
+    {
+        var expected = Guid.NewGuid();
+        var command = new RemoveProductFromCart();
+        _mediatorMock.Setup(m => m.Send(It.IsAny<RemoveProductFromCart>(), default))
+            .ReturnsAsync(expected);
+
+        var result = await _controller.RemoveProduct(command);
+
+        var ok = Assert.IsType<OkObjectResult>(result.Result);
+        Assert.Equal(expected, ok.Value);
+    }
+}
